d7350e4043487229dc75a0a6024e87c98fe67090
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-11-22 23:36:56 +0900

    refactor: rename spec

--- tests/main_spec.lua
+++ tests/main_spec.lua
@@ -1,14 +1,14 @@
 local exercism = require('exercism')
-local module = require('exercism.module')
+local main = require('exercism.main')
 
 describe('greet', function()
     it('returns message with config value', function()
         exercism.setup()
-        assert(module.greet() == 'Hello World!')
+        assert(main.greet() == 'Hello World!')
     end)
 
     it('returns message with user arg', function()
         exercism.setup()
-        assert(module.greet('Neovim') == 'Hello Neovim')
+        assert(main.greet('Neovim') == 'Hello Neovim')
     end)
 end)
