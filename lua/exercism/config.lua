22878f0258477195ade9cd8ac00feefc4a57abb9
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-23 10:40:57 +0530

    docs: better config annotations

--- lua/exercism/config.lua
+++ lua/exercism/config.lua
@@ -1,12 +1,14 @@
----@class exercism
+---@class ExercismConfig
 local M = {}
 
----@class exercism.config
+---@class ExercismConfigOptions
 ---@field exercism_workspace string
 ---@field default_language string
 ---@field add_default_keybindings boolean
 ---@field use_new_command boolean
 ---@field icons table<string, string>
+
+---@type ExercismConfigOptions
 local config = {
     exercism_workspace = '~/exercism',
     default_language = 'ruby',
@@ -18,10 +20,11 @@ local config = {
     },
 }
 
----@type exercism.config
+---@type ExercismConfigOptions
 M.config = config
 
----@param args exercism.config
+---Setup configuration with user options
+---@param args ExercismConfigOptions?
 M.setup = function(args)
     M.config = vim.tbl_deep_extend('force', M.config, args or {})
 end
